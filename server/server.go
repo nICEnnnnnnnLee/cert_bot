6b20452468ba346a07b4ba92d8b6d7ca188375ac
lijia <lijia0732@sina.com>
2024-09-11 08:35:43 +0800

    feat: 添加https支持

--- server/server.go
+++ server/server.go
@@ -9,7 +9,12 @@ import (
 	"syscall"
 )
 
-var UrlPrefix = GetEnvOr("UrlPrefix", "/xx")
+var (
+	UrlPrefix = GetEnvOr("UrlPrefix", "/xx")
+	bindAddr  = GetEnvOr("BindAddr", "127.0.0.1:8080")
+	certPath  = GetEnvOr("CertPath", "")
+	keyPath   = GetEnvOr("KeyPath", "")
+)
 
 func init() {
 	http.HandleFunc(UrlPrefix+"/api/config", func(w http.ResponseWriter, r *http.Request) {
@@ -39,21 +44,37 @@ func GetEnvOr(key string, defaultVal string) string {
 	}
 }
 
+func startServer(s *http.Server) error {
+	if certPath != "" && keyPath != "" {
+		return s.ListenAndServeTLS(certPath, keyPath)
+	} else {
+		return s.ListenAndServe()
+	}
+}
+
+type exitSig struct{}
+
+func (exitSig) String() string {
+	return "Exit signal"
+}
+func (exitSig) Signal() {}
+
 func Main() {
-	bindAddr := GetEnvOr("BindAddr", "127.0.0.1:8080")
+
 	log.Println("Running service at " + bindAddr)
 	server := &http.Server{
 		Addr: bindAddr,
 	}
-	go func() {
-		if err := server.ListenAndServe(); err != nil {
-			// panic(err)
-			log.Println(err)
-		}
-	}()
-
 	signalCh := make(chan os.Signal, 1)
 	signal.Notify(signalCh, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := startServer(server); err != nil {
+			// panic(err)
+			log.Println(err)
+			signalCh <- exitSig{}
+		}
+	}()
 
 	sig := <-signalCh
 	log.Printf("Received signal: %v\n", sig)
