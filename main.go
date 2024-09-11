a69444cd9de5eb309c68cb60de6c48a76daec9e7
lijia <lijia0732@sina.com>
2024-09-11 08:13:35 +0800

    feat: 兼容`server`和`cli`模式

--- main.go
+++ main.go
@@ -1,38 +1,15 @@
 package main
 
 import (
-	"context"
-	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-
+	"github.com/nicennnnnnnlee/cert_bot/cli"
 	"github.com/nicennnnnnnlee/cert_bot/server"
 )
 
 func main() {
-	bindAddr := server.GetEnvOr("BindAddr", "127.0.0.1:8080")
-	log.Println("Running service at " + bindAddr)
-	server := &http.Server{
-		Addr: bindAddr,
+	mode := server.GetEnvOr("Mode", "server")
+	if mode == "server" {
+		server.Main()
+	} else {
+		cli.Main()
 	}
-	go func() {
-		if err := server.ListenAndServe(); err != nil {
-			// panic(err)
-			log.Println(err)
-		}
-	}()
-
-	signalCh := make(chan os.Signal, 1)
-	signal.Notify(signalCh, syscall.SIGINT, syscall.SIGTERM)
-
-	sig := <-signalCh
-	log.Printf("Received signal: %v\n", sig)
-
-	if err := server.Shutdown(context.Background()); err != nil {
-		log.Fatalf("Server shutdown failed: %v\n", err)
-	}
-	log.Println("Server shutdown gracefully")
-
 }
--- server/server.go
+++ server/server.go
@@ -1,8 +1,12 @@
 package server
 
 import (
+	"context"
+	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 var UrlPrefix = GetEnvOr("UrlPrefix", "/xx")
@@ -33,4 +37,30 @@ func GetEnvOr(key string, defaultVal string) string {
 	} else {
 		return defaultVal
 	}
+}
+
+func Main() {
+	bindAddr := GetEnvOr("BindAddr", "127.0.0.1:8080")
+	log.Println("Running service at " + bindAddr)
+	server := &http.Server{
+		Addr: bindAddr,
+	}
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			// panic(err)
+			log.Println(err)
+		}
+	}()
+
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, syscall.SIGINT, syscall.SIGTERM)
+
+	sig := <-signalCh
+	log.Printf("Received signal: %v\n", sig)
+
+	if err := server.Shutdown(context.Background()); err != nil {
+		log.Fatalf("Server shutdown failed: %v\n", err)
+	}
+	log.Println("Server shutdown gracefully")
+
 }
